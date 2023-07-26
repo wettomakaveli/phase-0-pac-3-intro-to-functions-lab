b006f954b2a4100c6fb8df0c5f269ea182dc0808
koltenbinford <koltenman@gmail.com>
2023-07-26 10:32:53 -0700

    modified index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+  function logShout(string) {
+    console.log(string.toUpperCase());
+  }
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+function sayHiToHeadphonedRoommate(string) {
+  if (string === string.toLowerCase()) {
+    // Block of code
+    return("I can't hear you!")
+  }
+  if(string === string.toUpperCase()) {
+    //
+    return("YES INDEED!")
+  }
+  if (string === "Let's have dinner together!") {
+    //
+    return("I would love to!")
+  }
+}
+
+  
+const string = "Hello!";
+
+console.log(string);
+console.log(string.toLowerCase());
+
+console.log(string === string.toLowerCase());
